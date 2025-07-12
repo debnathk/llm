155bfba5ee6469057559857f12f1cb66b5a308ac
debnathk <debnathk1997@gmail.com>
2025-07-11 21:03:02 -0400

    Modify code

--- agents/main.py
+++ agents/main.py
@@ -14,11 +14,9 @@ class ResearchResponse(BaseModel):
     summary: str
     sources: list[str]
     tools_used: list[str]
+    
 
 llm = ChatOpenAI(model="gpt-4o-mini")
-# response = llm.invoke("What is earth?")
-# print(response)
-
 parser = PydanticOutputParser(pydantic_object=ResearchResponse)
 
 prompt = ChatPromptTemplate.from_messages(
@@ -26,9 +24,9 @@ prompt = ChatPromptTemplate.from_messages(
         (
             "system",
             """
-            You are a research assistant that will generate a research paper.
-            Answer the user query and use necessary tools.
-            Wrap the output in this format and provide no other text\n{format_instructions} 
+            You are a research assistant that will help generate a research paper.
+            Answer the user query and use neccessary tools. 
+            Wrap the output in this format and provide no other text\n{format_instructions}
             """,
         ),
         ("placeholder", "{chat_history}"),
@@ -37,8 +35,7 @@ prompt = ChatPromptTemplate.from_messages(
     ]
 ).partial(format_instructions=parser.get_format_instructions())
 
-tools = [search_tool, save_tool]
-
+tools = [search_tool, wiki_tool, save_tool]
 agent = create_tool_calling_agent(
     llm=llm,
     prompt=prompt,
@@ -46,9 +43,8 @@ agent = create_tool_calling_agent(
 )
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
-query = input("What can i help you research?")
+query = input("What can i help you research? ")
 raw_response = agent_executor.invoke({"query": query})
-# print(raw_response)
 
 try:
     structured_response = parser.parse(raw_response.get("output")[0]["text"])
