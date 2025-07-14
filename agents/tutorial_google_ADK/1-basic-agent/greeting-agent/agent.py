74043c764b1ae4a32a2ef92d53c40fe250222abc
debnathk <debnathk1997@gmail.com>
2025-07-14 00:56:16 -0400

    Complete 1-basic-agent

---
+++ agents/tutorial_google_ADK/1-basic-agent/greeting-agent/agent.py
@@ -0,0 +1,11 @@
+from google.adk.agents import Agent
+
+root_agent = Agent(
+    name="greeting-agent",
+    model="gemini-2.0-flash",
+    description="Greeting Agent",
+    instruction="""
+    You are a usful agent that greets the user.
+    Ask the user name and greet them by name.
+    """,
+)
