1cca402a571166aa9d9b18795f7bac8a528cbee2
debnathk <debnathk1997@gmail.com>
2025-07-14 01:21:35 -0400

    Correct spelling

--- agents/tutorial_google_ADK/1-basic-agent/greeting_agent/agent.py
+++ agents/tutorial_google_ADK/1-basic-agent/greeting_agent/agent.py
@@ -5,7 +5,7 @@ root_agent = Agent(
     model="gemini-2.0-flash",
     description="Greeting Agent",
     instruction="""
-    You are a usful agent that greets the user.
+    You are a useful agent that greets the user.
     Ask the user name and greet them by name.
     """,
 )
